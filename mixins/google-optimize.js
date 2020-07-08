d6df43c5f5968923c83f4e1e7e888ba9d7875525
Dan Siberry <dan@evolution7.com.au>
2020-07-08 16:40:23 +0200

    Add test for GO

---
+++ mixins/google-optimize.js
@@ -0,0 +1,57 @@
+import Vue from 'vue';
+
+export default {
+    data: () => ({
+        goLoaded: false,
+        params: null,
+        gaId: 'UA-172029636-1',
+        experimentsMapping: [
+            {
+                id: 'brOdoqbaS96pxl3ELtNR5Q',
+                handle: 'colourSwapTest',
+                description: '50/50 - Swap colour test to blue instead of default red.',
+                variants: ['0', '1']
+            },
+            {
+                id: 'brOdoq00096pxl3ELtNR5Q',
+                handle: 'experimentNotInOptimize',
+                description: 'Should show negative since this is not defined in the GO dashboard or is not an active experiment',
+                variants: ['0', '1']
+            }
+        ],
+        activeExperiments: {}
+    }),
+
+    computed: {
+        variantOverride() {
+            return this.params.get('go-variant');
+        }
+    },
+
+    mounted() {
+        this.params = new URLSearchParams(window.location.search);
+        this.checkForGoogleOptimize();
+    },
+
+    methods: {
+        checkForGoogleOptimize() {
+            if (window.google_optimize !== undefined) {
+                this.setActiveExperiements()
+                this.goLoaded = true;
+            }
+            else {
+                setTimeout(this.checkForGoogleOptimize, 100);
+            }
+        },
+
+        setActiveExperiements() {
+            this.experimentsMapping.forEach(experiment => {
+                const activeVariant = this.variantOverride || window.google_optimize.get(experiment.id);
+
+                if (experiment.variants.includes(activeVariant)) {
+                    Vue.set(this.activeExperiments, experiment.handle, activeVariant)
+                }
+            })
+        },
+    }
+};
---
+++ pages/index.vue
@@ -0,0 +1,108 @@
+<template>
+    <div class="container">
+        <div class="">
+            <h3 class="title">Google Optimize Test</h3>
+
+            <p><b>Experiments:</b></p>
+
+            <ul>
+                <li v-for="(experiment, i) in experimentsMapping" :key="i">
+                    <p>handle: {{ experiment.handle }}</p>
+                    <p>Experiment is live: {{ activeExperiments[experiment.handle] ? 'Yes' : 'No' }}</p>
+                    <p>id: {{ experiment.id }}</p>
+                </li>
+            </ul>
+
+            <div style="text-align: center">
+                <p><b>Colour swap test example.</b></p>
+                <p>Variants:  0 red (default), 1 blue</p>
+                <p>Active variant:  {{ activeExperiments['colourSwapTest'] }}</p>
+
+            </div>
+            <div v-if="goLoaded" class="test-marker" :class="swapColour ? 'bg-blue' : 'bg-red'"></div>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+import googleOptimize from '~/mixins/google-optimize';
+
+export default {
+  mixins: [googleOptimize],
+
+  computed: {
+    swapColour() {
+      return this.activeExperiments.colourSwapTest === '1';
+    }
+  }
+}
+</script>
+
+<style>
+
+.test-marker {
+  width: 100px;
+  height: 100px;
+  margin: 30px auto;
+  border-radius: 50%
+}
+
+ul {
+    margin-top: 10px;
+    margin-bottom: 60px;
+}
+
+li {
+    margin: 15px 0;
+}
+
+.bg-red {
+  background-color: red
+}
+
+.bg-blue {
+  background-color: blue
+}
+
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  margin-bottom: 20px;
+  font-weight: 300;
+  font-size: 60px;
+  color: #35495e;
+  letter-spacing: 1px;
+  text-align: center;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
